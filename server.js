7d6a5734334ffb21e21f30dc280e3bdff9b75442
sakau <sakau@usc.edu>
2022-06-22 20:01:46 -0700

    fixed router and server file

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
+app.use(routes);
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-
-  app.use(session(sess));
-
-  const helpers = require('./utils/helpers');
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
