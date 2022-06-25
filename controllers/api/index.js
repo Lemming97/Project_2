1a141a5644711eaaf5030ee82f41a42c16c24efd
Lindsey Martin <martinlindsey87@yahoo.com>
2022-06-25 13:18:54 -0700

    added seed template data into the seeds file. Updated server.js file with update routes and sequilize. Got server running with testing data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
-router.use('/users', userRoutes);
+router.use('/users', userRoutes);// this is linking to the user-routes.js file 
 
 module.exports = router;
