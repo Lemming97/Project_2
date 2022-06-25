7db81481022793ae980236b73abdc86ef8856165
Lindsey Martin <martinlindsey87@yahoo.com>
2022-06-25 15:06:01 -0700

    updaed seed files with data. Updated handlebar pages with new data and name. Added images

--- seeds/plantData.js
+++ seeds/plantData.js
@@ -1,27 +1,31 @@
 //plant images seed files 
 
-const { Plant } = require('../models');
+const {
+  Plant
+} = require('../models');
 
-const Plantdata = [
-  {
-    title: 'Blossoming Apricot',
-    artist: 'LedyX',
-    exhibition_date: 'March 30, 2018',
+const plantData = [{
+    title: 'Clary Sage',
+    common_name: 'Salvia Sclarea',
+    bloom_time: 'Spring to summer',
     gallery_id: 1,
-    filename: '01-blossoming-apricot.jpg',
-    description:
-      'Branches with pink apricot blossoms against a blue background.',
+    features: 'The strongly aromatic leaves of Clary Sage are used in a variety of industries! Young leaves are used to flavor wines, beers and liqueurs. "Mascatel oil" is an important essential oil extract that is used to perfume soaps and cosmetics. At home, the large woolly leaves make a nice accent for brightly colored plants in borders or mixed container planting. The flowers may be used to brew tea, added to salads or used as a garnish on the dinner plate.',
+    care_instructions: 'Feed annually with organic matter. Allow soil to dry between thorough waterings. Use light, well-drained soil. Does best in light, well-drained soil. Allow soil to dry between thorough waterings. Remove faded flowers.',
+    filename: 'clary_sage.jpeg',
+    description: 'Clary Sage - Salvia sclarea image',
   },
   {
-    title: 'Cosmos Flowers',
-    artist: 'WStudio',
-    exhibition_date: 'May 05, 2017',
+    title: 'Cilantro',
+    common_name: 'Coriandrum Sativum',
+    bloom_time: 'Spring',
     gallery_id: 1,
-    filename: '02-cosmos-flowers.jpg',
-    description: 'Pink cosmos flowers against a blue sky.',
+    features: 'This herb produces a continual crop of piquant-flavored leaves for one season. This selection is slow to bolt. Bolting is when the central stalk of a plant grows quickly upward and produces flowers, which turns the flavor bitter. As a result, slow to bolt is a desirable feature in Cilantro. Use the tender leaves to add a citrus-like taste to salsa, salads or guacamole.',
+    care_instructions: 'Plant feed is not necessary. Allow soil to dry between thorough waterings. Use light, well-drained soil. Does best in light, well-drained soil. Allow soil to dry between thorough waterings. Harvest foliage as needed. Harvest seeds when they turn brown.',
+    filename: 'cilantro.jpeg',
+    description: 'up close photo of cilantro in a pot',
   }
 ];
 
-const seedPlants = () => Plant.bulkCreate(Plantdata); 
+const seedPlants = () => Plant.bulkCreate(plantData);
 
 module.exports = seedPlants;
