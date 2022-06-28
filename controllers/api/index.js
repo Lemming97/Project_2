43da76ce02d467fd357747d9ebfc83bdbf3ec7a3
sakau <sakau@usc.edu>
2022-06-27 19:19:30 -0700

    unfinished

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const User = require('../../models/User.js');
 const Post = require('../../models/Post');
+const Plant = require('../../models/Plant');
+const Gallery = require('../../models/Gallery');
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const homeRoutes = require('./home-routes');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -13,7 +16,17 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+Gallery.hasMany(Plant, {
+    foreignKey: 'gallery_id',
+  });
+  
+Plant.belongsTo(Gallery, {
+    foreignKey: 'gallery_id',
+});
+  
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/home', homeRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,13 +3,5 @@ const Gallery = require('./Gallery');
 const Plant = require('./Plant');
 const Post = require('./Post')
 
-Gallery.hasMany(Plant, {
-    foreignKey: 'gallery_id',
-  });
-  
-  Plant.belongsTo(Gallery, {
-    foreignKey: 'gallery_id',
-  });
-  
 
 module.exports = { User, Gallery, Plant, Post };
