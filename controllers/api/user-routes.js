7951cb876a6bf097cef9898fee0d1e8b988a4a03
Lindsey Martin <martinlindsey87@yahoo.com>
2022-07-01 11:46:58 -0700

    Updated handlebars blogs page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,10 +3,8 @@ const {
   User
 } = require('../../models');
 
-// GET /api/users
+// Get all users info /api/users
 router.get('/', (req, res) => {
-
-  console.log(req.session);
   // Access our User model and run .findAll() method)
   User.findAll()
     .then(dbUserData => res.json(dbUserData))
@@ -16,7 +14,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET /api/users/1
+// Get a single user info /api/users/?
 router.get('/:id', (req, res) => {
   User.findOne({
       where: {
@@ -38,7 +36,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// POST /api/users /create user
+// Create a user /api/users
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -46,18 +44,14 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-    .then(dbUserData => {
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-
-        res.json(dbUserData);
-      });
-    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// POST /api/users LOGIN /api
+// Login route
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
@@ -66,39 +60,28 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({
+        message: 'No user with that email address!'
+      });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({
+        message: 'Incorrect password!'
+      });
       return;
     }
 
-    req.session.save(() => {
-      // declare session variables
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    res.json({
+      user: dbUserData,
+      message: 'You are now logged in!'
     });
   });
 });
 
 
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  }
-  else {
-    res.status(404).end();
-  }
-});
 
 // Update a user /api/users/1
 router.put('/:id', (req, res) => {
@@ -126,7 +109,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// DELETE /api/users/1
+// Delete a user /api/users/1
 router.delete('/:id', (req, res) => {
   User.destroy({
       where: {
