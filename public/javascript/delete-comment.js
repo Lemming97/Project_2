ded2b2070a00ec83b072419f61ec5aa05e3d959f
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-06 12:07:35 -0700

    Fixed delete comment route and ability to see button if user is owner

--- public/javascript/delete-comment.js
+++ public/javascript/delete-comment.js
@@ -1,25 +1,29 @@
-const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
-const deleteBtn = document.querySelector(`#deletecommentbtn${id}`);
-const comment = document.querySelector('#comment');
+// const postID = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
-async function deleteFormHandler(event) {
-  event.preventDefault();
-  
-  
-  
-  
-  console.log(id)
-    const response = await fetch(`api/post/${id}`, {
+const deleteBtns = document.querySelectorAll('.delete-comment-btn');
+
+const comment = document.querySelector('.comment');
+
+deleteBtns.forEach(deleteBtn => {
+
+
+  const commentID = deleteBtn.getAttribute('data-id');
+
+  deleteBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+
+    console.log(commentID)
+    const response = await fetch(`/api/comments/${commentID}`, {
       method: 'DELETE'
     });
- 
+
     if (response.ok) {
- 
+
       document.location.reload();
     } else {
       alert(response.statusText);
     }
 
-}
-
-deleteBtn.addEventListener('click', deleteFormHandler);
+  });
+});
