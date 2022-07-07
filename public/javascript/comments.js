2744d7f912ca315e9f7b082765fd6e26a22fdc04
schai2009 <schai20100@gmail.com>
2022-07-06 20:20:38 -0700

    updated fetch function on comments.js

--- public/javascript/comments.js
+++ public/javascript/comments.js
@@ -1,24 +1,26 @@
 async function commentFormHandler(event) {
   event.preventDefault();
 
-  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  const comment_text = document
+    .querySelector('textarea[name="comment-body"]')
+    .value.trim();
 
   const post_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
 
   if (comment_text) {
-    const response = await fetch('http://localhost:3001/api/comments', {
+    const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({
         comment_text,
-        post_id
+        post_id,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     });
-  
+
     if (response.ok) {
       document.location.reload();
     } else {
@@ -27,4 +29,6 @@ async function commentFormHandler(event) {
   }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', commentFormHandler);
