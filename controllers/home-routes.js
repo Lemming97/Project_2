bc3e622953d5bc6ba4c9205d930b820a4fd0ffea
schai2009 <schai20100@gmail.com>
2022-07-06 17:46:14 -0700

    erged main

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,5 @@
 const router = require('express').Router();
-const {
-  Gallery,
-  Plant,
-  Post,
-  User,
-  Comment
-} = require('../models');
+const { Gallery, Plant, Post, User, Comment } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 const dayjs = require('dayjs');
@@ -14,24 +8,23 @@ const formatDate = () => {
   const rightNow = new Date();
   // currentDay = dayjs(rightNow).format('MMMM D YYYY');
   return dayjs(rightNow).format('MMMM D YYYY');
-}
-
-
-
+};
 
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   try {
     const dbGalleryData = await Gallery.findAll({
-      include: [{
-        model: Plant,
-        attributes: ['filename', 'description'],
-      }, ],
+      include: [
+        {
+          model: Plant,
+          attributes: ['filename', 'description'],
+        },
+      ],
     });
 
     const galleries = dbGalleryData.map((gallery) =>
       gallery.get({
-        plain: true
+        plain: true,
       })
     );
 
@@ -50,28 +43,12 @@ router.get('/', async (req, res) => {
       })
     );
 
-    const dbPostsData = await Post.findAll({
-      include: [{
-        model: User,
-        attributes: ['username'],
-      }]
-    });
-
-    const posts = dbPostsData.map((post) =>
-      post.get({
-        plain: true
-      })
-    );
-
     res.render('homepage', {
       galleries,
       posts,
       loggedIn: req.session.loggedIn,
       currentDay: formatDate(),
     });
-      currentDay: formatDate()
-    });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -102,29 +79,11 @@ router.get('/gallery/:id', withAuth, async (req, res) => {
 
     const gallery = dbGalleryData.get({
       plain: true,
-      include: [{
-        model: Plant,
-        attributes: [
-          'id',
-          'title',
-          'family_name',
-          'bloom_time',
-          'features',
-          'care_instructions',
-          'filename',
-          'description',
-        ],
-      }, ],
-    });
-
-    const gallery = dbGalleryData.get({
-      plain: true
     });
     res.render('plantGallery', {
       gallery,
       loggedIn: req.session.loggedIn,
       currentDay: formatDate(),
-      currentDay: formatDate()
     });
   } catch (err) {
     console.log(err);
@@ -140,15 +99,12 @@ router.get('/plant/:id', withAuth, async (req, res) => {
 
     const plant = dbPlantData.get({
       plain: true,
-      plain: true
     });
 
     res.render('plant', {
       plant,
       loggedIn: req.session.loggedIn,
       currentDay: formatDate(),
-      currentDay: formatDate()
-
     });
   } catch (err) {
     console.log(err);
@@ -189,45 +145,16 @@ router.get('/post/:id', withAuth, async (req, res) => {
     const post = dbPostdata.get({
       plain: true,
     });
-
-    res.render('single-post', { post, loggedIn: req.session.loggedIn });
-  try {
-    const dbPostdata = await Post.findByPk(req.params.id, {
-      include: [{
-          model: Comment,
-          attributes: [
-            'id',
-            'comment_text',
-            'user_id',
-          ],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: [
-            'username'
-          ]
-        }
-      ]
-    });
-
-    const post = dbPostdata.get({
-      plain: true
-    });
     post.comments = post.comments.map((comment) => ({
       ...comment,
       isOwner: req.session.user_id === comment.user_id,
-    }))
+    }));
 
     res.render('single-post', {
       post,
       loggedIn: req.session.loggedIn,
-      currentDay: formatDate()
+      currentDay: formatDate(),
     });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -257,26 +184,6 @@ router.get('/posts', async (req, res) => {
       loggedIn: req.session.loggedIn,
       currentDay: formatDate(),
     });
-
-    const dbPostsData = await Post.findAll({
-      include: [{
-        model: User,
-        attributes: ['username'],
-      }]
-    });
-
-    const posts = dbPostsData.map((post) =>
-      post.get({
-        plain: true
-      })
-    );
-
-    res.render('blogs', {
-      posts,
-      loggedIn: req.session.loggedIn,
-      currentDay: formatDate()
-    });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -285,40 +192,33 @@ router.get('/posts', async (req, res) => {
 
 router.get('/comments', async (req, res) => {
   try {
-
     const dbCommentsData = await Comment.findAll({
-      include: [{
-          attributes: [
-            'id',
-            'comment_text',
-            'user_id',
-          ],
+      include: [
+        {
+          attributes: ['id', 'comment_text', 'user_id'],
           include: {
             model: User,
-            attributes: ['username']
-          }
+            attributes: ['username'],
+          },
         },
         {
           model: User,
-          attributes: [
-            'username'
-          ]
-        }
-      ]
+          attributes: ['username'],
+        },
+      ],
     });
 
     const comments = dbCommentsData.map((post) =>
       comments.get({
-        plain: true
+        plain: true,
       })
     );
 
     res.render('comment', {
       comments,
       loggedIn: req.session.loggedIn,
-      currentDay: formatDate()
+      currentDay: formatDate(),
     });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -328,35 +228,29 @@ router.get('/comments', async (req, res) => {
 router.get('/comments/:id', async (req, res) => {
   try {
     const dbCommentdata = await Comment.findByPk(req.params.id, {
-      include: [{
-          attributes: [
-            'id',
-            'comment_text',
-            'user_id',
-          ],
+      include: [
+        {
+          attributes: ['id', 'comment_text', 'user_id'],
           include: {
             model: User,
-            attributes: ['username']
-          }
+            attributes: ['username'],
+          },
         },
         {
           model: User,
-          attributes: [
-            'username'
-          ]
-        }
-      ]
+          attributes: ['username'],
+        },
+      ],
     });
 
     const comment = dbCommentdata.get({
-      plain: true
+      plain: true,
     });
 
     res.render('comment', {
       comment,
-      loggedIn: req.session.loggedIn
+      loggedIn: req.session.loggedIn,
     });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -364,4 +258,3 @@ router.get('/comments/:id', async (req, res) => {
 });
 
 module.exports = router;
-
