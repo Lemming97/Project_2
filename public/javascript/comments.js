506b335196e3b81f159d5f3549bfe092a2be8aae
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-06 20:22:31 -0700

    created heroku branch

--- public/javascript/comments.js
+++ public/javascript/comments.js
@@ -8,7 +8,7 @@ async function commentFormHandler(event) {
   ];
 
   if (comment_text) {
-    const response = await fetch('http://localhost:3001/api/comments', {
+    const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({
         comment_text,
